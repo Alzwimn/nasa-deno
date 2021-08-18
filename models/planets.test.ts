d72066b878d8777c2a4ec9bcb7a5e7a63ec33b8c
Alzwimn <alzwimn@gmail.com>
2021-08-18 13:38:58 -0300

    refactor: improve code testability

---
+++ models/planets.test.ts
@@ -0,0 +1,7 @@
+/**
+ * Deno includes: 
+ * 
+ * 1. The test runner in the CLI.
+ * 2. Assertions in the standard library
+ * 3. Built-in test fixtures with Deno.test().
+ */
--- models/planets.ts
+++ models/planets.ts
@@ -7,16 +7,8 @@ type Planet = Record<string, string>
 
 let planets: Planet[];
 
-async function loadPlanetsData() {
-  // console.time('[loadPlanetsData] took');
-  const path = join("data", "kepler_exoplanets_nasa.csv");
-
-  const file = await Deno.open(path);
-  const bufReader = new BufReader(file);
-  const result = await parse(bufReader, { comment: "#", skipFirstRow: true}) as Planet[];
-  Deno.close(file.rid);
-
-  const habiTablePlanets = result.filter((planet) => {
+function filterHabitablePlanets(planets: Planet[]) {
+  return planets.filter((planet) => {
     const planetaryRadius = Number(planet["koi_prad"]);
     const stellarMass = Number(planet["koi_smass"]);
     const stellarRadius = Number(planet["koi_srad"]);
@@ -25,7 +17,17 @@ async function loadPlanetsData() {
     && stellarMass > 0.78 && stellarMass < 1.04
     && stellarRadius > 0.99 && stellarRadius < 1.01;
   });
-  // console.timeEnd('[loadPlanetsData] took');
+}
+
+async function loadPlanetsData() {
+  const path = join("data", "kepler_exoplanets_nasa.csv");
+
+  const file = await Deno.open(path);
+  const bufReader = new BufReader(file);
+  const result = await parse(bufReader, { comment: "#", skipFirstRow: true}) as Planet[];
+  Deno.close(file.rid);
+
+  const habiTablePlanets = filterHabitablePlanets(result as Planet[]);
   return habiTablePlanets.map((planet) => {
     return _.pick(planet, [
       "koi_prad",
